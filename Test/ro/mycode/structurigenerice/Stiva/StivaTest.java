04ef1c3b882b6d61808f6c8f1d7725eb3b583581
Valentin Florea <vflorea97@yahoo.com>
2022-11-23 19:41:59 +0200

    final stiva

---
+++ Test/ro/mycode/structurigenerice/Stiva/StivaTest.java
@@ -0,0 +1,122 @@
+package ro.mycode.structurigenerice.Stiva;
+
+import org.junit.jupiter.api.Test;
+import ro.mycode.Model.Masina;
+import ro.mycode.structurigenerice.List;
+
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StivaTest {
+
+    @Test
+    void push() {
+        Masina masina = new Masina("Logan","Dacia",2010,true,"Benzina",1.6);
+        Masina masina1 = new Masina("A3","Audi",2012,true,"Motorina",1.8);
+        Masina masina2 = new Masina("X5","BMW",2018,true,"Motorina",1.8);
+        Masina masina3 = new Masina("2008","Peugeot",2020,true,"Benzina",1.6);
+
+        Stack<Masina> masinas = new Stack<>();
+        masinas.push(masina);
+        masinas.push(masina1);
+        masinas.push(masina2);
+        masinas.push(masina3);
+
+        System.out.println(masinas.toString());
+    }
+    @Test
+    void pop() {
+        Masina masina = new Masina("Logan","Dacia",2010,true,"Benzina",1.6);
+        Masina masina1 = new Masina("A3","Audi",2012,true,"Motorina",1.8);
+        Masina masina2 = new Masina("X5","BMW",2018,true,"Motorina",1.8);
+        Masina masina3 = new Masina("2008","Peugeot",2020,true,"Benzina",1.6);
+
+        Stack<Masina> masinas = new Stack<>();
+        masinas.push(masina);
+        masinas.push(masina1);
+        masinas.push(masina2);
+        masinas.push(masina3);
+
+        masinas.pop();
+
+        System.out.println(masinas.toString());
+    }
+
+    @Test
+    void size() {
+        Masina masina = new Masina("Logan","Dacia",2010,true,"Benzina",1.6);
+        Masina masina1 = new Masina("A3","Audi",2012,true,"Motorina",1.8);
+        Masina masina2 = new Masina("X5","BMW",2018,true,"Motorina",1.8);
+        Masina masina3 = new Masina("2008","Peugeot",2020,true,"Benzina",1.6);
+
+        Stack<Masina> masinas = new Stack<>();
+        masinas.push(masina);
+        masinas.push(masina1);
+        masinas.push(masina2);
+        masinas.push(masina3);
+
+        int size = masinas.size();
+
+        assertEquals(4,size);
+
+    }
+
+    @Test
+    void isEmpty() {
+        Stack<Masina> masinas = new Stack<>();
+
+        boolean isEmpty = masinas.isEmpty();
+
+        assertEquals(true,isEmpty);
+    }
+
+    @Test
+    void peek() {
+        Masina masina = new Masina("Logan","Dacia",2010,true,"Benzina",1.6);
+        Masina masina1 = new Masina("A3","Audi",2012,true,"Motorina",1.8);
+        Masina masina2 = new Masina("X5","BMW",2018,true,"Motorina",1.8);
+        Masina masina3 = new Masina("2008","Peugeot",2020,true,"Benzina",1.6);
+
+        Stack<Masina> masinas = new Stack<>();
+        masinas.push(masina);
+        masinas.push(masina1);
+        masinas.push(masina2);
+        masinas.push(masina3);
+
+        Masina masina4 = masinas.peek();
+
+        assertEquals(masina3,masina4);
+        System.out.println(masinas.toString());
+
+    }
+    @Test
+    void  pb(){
+        //sa verificam daca un text este corect balantat folosind stiva
+        //"asdsad(asdadsa)asdasd)("
+        String text = "Ana (()()(are)(mai multe)) mere {[}}decat Andrei";
+
+        Stiva<Character> characterStiva = new Stiva<>();
+
+        boolean flag=true;
+         for(int i=0;i<text.length();i++){
+             if(text.charAt(i)=='('){
+
+                 characterStiva.push(text.charAt(i));
+             }else if(text.charAt(i)==')'){
+
+                    if(characterStiva.isEmpty()){
+                        flag=false;
+                    }else{
+
+                        characterStiva.pop();
+                    }
+             }
+         }
+         if(characterStiva.isEmpty()&&flag==true){
+             System.out.println("textul este corect balantat");
+         }else{
+             System.out.println("Nu este balantat");
+         }
+    }
+}
---
+++ src/ro/mycode/structurigenerice/Stiva/Stiva.java
@@ -0,0 +1,46 @@
+package ro.mycode.structurigenerice.Stiva;
+
+import ro.mycode.structurigenerice.Nod;
+
+public class Stiva<K> {
+
+    private Nod<K> head = null;
+
+    public void push(K k){
+        if (head == null){
+            head = new Nod<>(null, k);
+        }else {
+            Nod<K> nod = new Nod<>();
+            nod.setData(k);
+            nod.setNext(head);
+            head = nod;
+        }
+    }
+
+    public void pop(){
+        head = head.getNext();
+    }
+    public int size(){
+        Nod<K> temp = head;
+        int size = 0;
+        while (temp != null){
+            temp = temp.getNext();
+            size++;
+        }
+        return size;
+    }
+    public boolean isEmpty(){
+        if (size() == 0){
+            return true;
+        }
+        return false;
+
+    }
+    public K peek(){
+        Nod<K> temp = head;
+        while (temp != null){
+            temp = temp.getNext();
+        }
+        return temp.getData();
+    }
+}
